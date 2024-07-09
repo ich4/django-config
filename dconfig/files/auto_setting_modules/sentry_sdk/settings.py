0b92701c44dc61201d3ef16a20c5690e5a27c785
Anne <morakot@unixdev.co.th>
2024-07-09 22:57:52 +0700

    fix: add environment configurations in sentry settings

--- dconfig/files/auto_setting_modules/sentry_sdk/settings.py
+++ dconfig/files/auto_setting_modules/sentry_sdk/settings.py
@@ -1,14 +1,24 @@
 
-RAVEN_DSN = getattr(configs, 'RAVEN_DSN', False)
-if RAVEN_DSN:
+SENTRY_DSN = getattr(configs, 'SENTRY_DSN', False)
+SENTRY_ENVIRONMENT = getattr(configs, 'SENTRY_ENVIRONMENT', 'production')
+SENTRY_ENABLE_TRACING = getattr(configs, 'SENTRY_ENABLE_TRACING', False)
+SENTRY_TRACES_SAMPLE_RATE = getattr(configs, 'SENTRY_TRACES_SAMPLE_RATE', 1.0)
+SENTRY_SEND_DEFAULT_PII = getattr(configs, 'SENTRY_SEND_DEFAULT_PII', True)
+
+
+if SENTRY_DSN:
     import sentry_sdk
 
     from sentry_sdk.integrations.django import DjangoIntegration
 
+
     sentry_sdk.init(
-        RAVEN_DSN,
+        SENTRY_DSN,
         integrations=[DjangoIntegration()],
-        send_default_pii=True,
+        send_default_pii=SENTRY_SEND_DEFAULT_PII,
+        enable_tracing=SENTRY_ENABLE_TRACING,
+        environment=SENTRY_ENVIRONMENT,
+        traces_sample_rate=SENTRY_TRACES_SAMPLE_RATE,
     )
 
     LOGGING['formatters']['verbose']  = {
